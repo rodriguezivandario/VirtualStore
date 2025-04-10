021d403eaabb0ef13830ffa52c272bc5b7742412
rodriguezivandario <rodriguezivandario93@gmail.com>
2025-04-10 13:30:38 -0500

    version2

---
+++ app.js
@@ -0,0 +1,24 @@
+let x = 10; // tipo de dato entero
+let y = 3.14;
+let nombre = "Romulo b";
+let esactivo = true;
+
+if (x > y) {
+console.log("el numero mayor es x",x);
+} else {
+console.log("el numero mayor es y",y);
+}
+
+
+
+
+
+x = 1;
+while (x <= 10) {
+console.log("soy el numero",x)
+x++;
+}
+
+for (y = 1; y <= 10; y++) {
+    console.log("soy el numero",y);
+}
